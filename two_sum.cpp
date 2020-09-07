726bcb265a2cca7e85a8b3f119734c24e8c35f8e
vhgnguyen <43420326+vhgnguyen@users.noreply.github.com>
2020-09-07 20:31:07 +0200

    first commit

---
+++ two_sum.cpp
@@ -0,0 +1,18 @@
+#include <algorithm>
+
+struct ListNode
+{
+    /* data */
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution{
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode* l3 = NULL;
+        
+    }
+}
+
